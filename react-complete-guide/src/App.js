0cd6dcdf8fd6b06d761b83aed37d6bea62a84315
Leonardo Souza <leogsouza@gmail.com>
2019-01-12 15:48:29 -0200

    Handling props and state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Léo", age: 35},
+      { name: "Fabi", age: 32},
+      { name: "Guel", age: 18},
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState( {
+      persons: [
+        { name: "Leonardo", age: 36},
+        { name: "Fabiana", age: 37},
+        { name: "Miguel", age: 7},
+      ] 
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Cooking</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
       </div>
     );
   }
