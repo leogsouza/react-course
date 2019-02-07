14b88ccccca092a85fe316b6461561942df0b0b9
Leonardo Souza <leogsouza@gmail.com>
2019-02-06 22:52:14 -0200

    Improving performance

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../../hoc/Wrapper';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingregientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render() {
+    const ingregientSummary = Object.keys(this.props.ingredients)
     .map(ingKey => {
       return (
         <li key={ingKey}>
-          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
         </li>);
     })
 
-
-  return (
-    <Wrapper>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
-      <ul>
-        {ingregientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout!</p>
-      <Button
-        btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button      
-        btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Wrapper>
-  )
-};
-
-export default orderSummary;
+    return (
+      <Wrapper>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients</p>
+        <ul>
+          {ingregientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout!</p>
+        <Button
+          btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button      
+          btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Wrapper>
+    ) 
+  }
+}
+export default OrderSummary;
