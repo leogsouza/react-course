2bb75e84fa98f4b4169d88030ed8ce4bf1bb7835
Leonardo Souza <leogsouza@gmail.com>
2019-07-17 19:17:47 -0300

    Passing ingredients via query params

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -105,7 +105,17 @@ class BurgerBuilder extends Component {
     //   .post("/orders.json", order)
     //   .then(response => this.setState({ loading: true, purchasing: false }))
     //   .catch(error => this.setState({ loading: true, purchasing: false }));
-    this.props.history.push('/checkout');
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '='+encodeURIComponent(this.state.ingredients[i]));      
+    }
+    const queryString = queryParams.join('&')
+
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   };
 
   render() {
