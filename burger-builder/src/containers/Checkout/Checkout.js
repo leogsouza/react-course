4a5923ea6ef31e963047ea3b872fee892c090018
Leonardo Souza <leogsouza@gmail.com>
2019-07-17 18:56:59 -0300

    Navigating Back and To Next Page

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 
 class Checkout extends Component {
-
   state = {
     ingredients: {
       salad: 1,
@@ -11,16 +10,26 @@ class Checkout extends Component {
       cheese: 1,
       bacon: 1
     }
+  };
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
   }
 
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary 
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+          ingredients={this.state.ingredients} />
       </div>
-    )
+    );
   }
-
 }
 
 export default Checkout;
