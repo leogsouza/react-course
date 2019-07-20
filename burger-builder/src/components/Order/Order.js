66dafdd27ab555ae90b1db5e245ff421af05db28
Leonardo Souza <leogsouza@gmail.com>
2019-07-20 20:26:47 -0300

    Outputting the orders

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Order.module.css';
+import classes from "./Order.module.css";
 
-const order = (props) =>  (
-  <div className={classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: USD 5.00</p>
-  </div>
-);
+const order = props => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    })
+  }
+
+  const ingredientOutput = ingredients.map( ingredient => {
+    return (
+      <span className={classes.Ingredient}
+        key={ingredient.name}>{ingredient.name} ({ingredient.amount})
+      </span>
+    );
+  })
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>
+        Price: <strong>USD {props.price.toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default order;
